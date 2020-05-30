6aab67b1a2c7493d2f7d487aad7f6a30c0d0df1b
bulldozerat <bulldozerat@gmail.com>
2020-05-30 09:28:45 +0300

    cmd fetch data

--- src/pages/Albums/Albums.jsx
+++ src/pages/Albums/Albums.jsx
@@ -1,6 +1,18 @@
 import React, { useEffect } from 'react';
 
-const Albums = () => <h1>123</h1>;
+const Albums = () => {
+  useEffect(() => {
+    fetchAlbumbsData();
+  }, []);
+
+  const fetchAlbumbsData = async () => {
+    await fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  };
+
+  return <h1>123</h1>;
+};
 
 Albums.propTypes = {};
 
